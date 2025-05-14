40b5009570d142783087e146d83a66bb4e9d3754
thuandang97-dev <thuandang97.dev@gmail.com>
2025-05-14 11:29:14 +0700

    update image

---
+++ public/js/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBdnxBo18HkMesTOSUqQBEQEhPsSN4Y7Mw",
+  authDomain: "wishes-8020f.firebaseapp.com",
+  databaseURL:
+    "https://wishes-8020f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wishes-8020f",
+  storageBucket: "wishes-8020f.appspot.com",
+  messagingSenderId: "703372735927",
+  appId: "1:703372735927:web:4ed59944b142db5a0f6432",
+  measurementId: "G-2CWZRCED9N",
+};
+
+export default firebaseConfig;
